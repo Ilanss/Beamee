0f2477311bb541db3c9b3ef3bd0c4ad366395f82
Ilanss <jeremie_pro@hotmail.com>
2026-04-28 10:45:56 +0200

    Polishing + PDF export (#12)
    * Change app description
    
    * Polish keyboard nav
    
    * Add esc as shortcut to stop projecting
    
    * Handle main window closing
    
    * Specify Windows build filename
    
    * Implements song PDF exports

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -658,8 +658,7 @@ function renderSongView(songData) {
 
         li.addEventListener('click', () => {
             currentVerseIndex = i;
-            document.querySelectorAll("#verse-display ul li").forEach((e) => e.classList.remove("bg-base-300"));
-            li.classList.add("bg-base-300");
+            setActiveVerseHighlight(i);
             updateProjection();
         });
 
@@ -1320,6 +1319,7 @@ export async function mount(root, context = {}) {
 
     onIpc('black-screen', () => {
         currentVerseIndex = undefined;
+        setActiveVerseHighlight(undefined);
         if (previewLyrics) {
             previewLyrics.replaceChildren();
         }
@@ -1394,6 +1394,7 @@ export async function mount(root, context = {}) {
 
     onIpc('verse:change', (verse) => {
         currentVerseIndex = verse;
+        setActiveVerseHighlight(verse);
         updateProjection();
     });
 
@@ -1408,12 +1409,6 @@ export async function mount(root, context = {}) {
     });
 
     on(rootElement.querySelector('#black-screen'), 'click', () => {
-        if (previewLyrics) {
-            previewLyrics.replaceChildren();
-        }
-
-        document.querySelectorAll("#verse-display ul li").forEach((e) => e.classList.remove("bg-base-300"));
-        currentVerseIndex = undefined;
         ipcRenderer.send('black-screen');
     });
 
@@ -2476,20 +2471,25 @@ function updateProjection() {
     ipcRenderer.send('display-lyrics', verseText);
 }
 
+function setActiveVerseHighlight(index) {
+    document.querySelectorAll("#verse-display ul li").forEach((e) => e.classList.remove("bg-base-300"));
+    if (index !== undefined) {
+        document.querySelector("#verse-" + index)?.classList.add("bg-base-300");
+    }
+}
+
 function changeToPrevVerse() {
     if (currentVerseIndex !== undefined && currentVerseIndex > 0) {
-        document.querySelectorAll("#verse-display ul li").forEach((e) => e.classList.remove("bg-base-300"));
         currentVerseIndex--;
-        document.querySelector("#verse-" + currentVerseIndex).classList.add("bg-base-300");
+        setActiveVerseHighlight(currentVerseIndex);
         updateProjection();
     }
 }
 
 function changeToNextVerse() {
     if (currentVerseIndex !== undefined && Array.isArray(currentLyrics) && currentVerseIndex < currentLyrics.length - 1) {
-        document.querySelectorAll("#verse-display ul li").forEach((e) => e.classList.remove("bg-base-300"));
         currentVerseIndex++;
-        document.querySelector("#verse-" + currentVerseIndex).classList.add("bg-base-300");
+        setActiveVerseHighlight(currentVerseIndex);
         updateProjection();
     }
 }
@@ -2506,5 +2506,6 @@ function changeToChorus() {
     }
 
     currentVerseIndex = chorusIndex;
+    setActiveVerseHighlight(chorusIndex);
     updateProjection();
 }
